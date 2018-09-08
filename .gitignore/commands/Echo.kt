6a9d16710245ea7b8c23ab21fbdf108171ea8595
Neexona <37214268+Neexona@users.noreply.github.com>
2018-09-08 14:02:43 +0200

    Add files via upload

---
+++ .gitignore/commands/Echo.kt
@@ -0,0 +1,16 @@
+package me.alexflipnote.kawaiibot.commands
+
+import me.aurieh.ichigo.core.CommandContext
+import me.aurieh.ichigo.core.ICommand
+import me.aurieh.ichigo.core.annotations.Command
+
+@Command(description = "Echo!")
+class Echo : ICommand {
+    override fun run(ctx: CommandContext) {
+        if (ctx.argString.isEmpty()) {
+            ctx.send("Nothing will echo if you don't give me something to say... ;-;")
+        } else {
+            ctx.send("**${ctx.author.name}**: ${ctx.args.asCleanString}")
+        }
+    }
+}
